807b9580e135dfba6ad6713e0efe4a8be1fa510a
Uchenna Ukeh <37188437+uchennaukeh@users.noreply.github.com>
2026-07-14 00:13:30 -0700

    Implement routing for API requests
    updating routing rules

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,4 +1,34 @@
 
+<!-- 
+// direct reqested route
 $route = $_SERVER['REQUEST_URI'];
+echo $route; 
+-->
 
-echo $route;
+$route = str_replace('/api/v1/', '', $_SERVER['REQUEST_URI']);
+$route = trim($route, '/');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch (true) {
+
+    case $method === 'POST' && $route === 'auth/login':
+        require 'controllers/auth/login.php';
+        break;
+
+    case $method === 'GET' && $route === 'members':
+        require 'controllers/members/index.php';
+        break;
+
+    case $method === 'POST' && $route === 'members':
+        require 'controllers/members/create.php';
+        break;
+
+    default:
+        http_response_code(404);
+
+        echo json_encode([
+            'success' => false,
+            'message' => 'Route not found'
+        ]);
+}
