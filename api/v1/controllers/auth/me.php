5afed7c2ecfbfea23d66fddb9b8775f094fa08cc
Uchenna Ukeh <37188437+uchennaukeh@users.noreply.github.com>
2026-07-14 09:19:43 -0700

    Sanitize user data in response
    Remove sensitive fields from user response

--- api/v1/controllers/auth/me.php
+++ api/v1/controllers/auth/me.php
@@ -1,4 +1,3 @@
-
 <?php
 
 header('Content-Type: application/json');
@@ -10,6 +9,9 @@ require_once '../../helpers/auth.php';
 $user = getAuthenticatedUser();
 
 unset($user['api_token']);
+unset($user['password']);
+unset($user['reset_token']);
+unset($user['reset_token_expires']);
 
 successResponse(
     'User retrieved successfully',
