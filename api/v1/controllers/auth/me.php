24fdc96b7ba70fb95b73ef92dc0f24a4b6f55cc6
Uchenna Ukeh <37188437+uchennaukeh@users.noreply.github.com>
2026-07-14 08:36:54 -0700

    Retrieve authenticated user details without token

--- api/v1/controllers/auth/me.php
+++ api/v1/controllers/auth/me.php
@@ -1,1 +1,17 @@
 
+<?php
+
+header('Content-Type: application/json');
+
+require_once '../../config/db.php';
+require_once '../../helpers/response.php';
+require_once '../../helpers/auth.php';
+
+$user = getAuthenticatedUser();
+
+unset($user['api_token']);
+
+successResponse(
+    'User retrieved successfully',
+    $user
+);
